1b326ee8f15ff1a10ef3107b22f356132f63b64d
Ing. Esteban Correa Pereira <estebancorrea405@gmail.com>
2023-01-21 09:58:01 -0500

    third commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Controller, Post, Body} from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, SetMetadata } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
+import { GetUser } from './decorators/get-user.decorador';
+import { RoleProtected } from './decorators/role-protected.decorator';
 import { LoginUserDto, CreateUserDto } from './dto/index';
+import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
+import { ValidRoles } from './interfaces';
 
 @Controller('auth')
 export class AuthController {
@@ -16,5 +22,20 @@ export class AuthController {
     return this.authService.login( loginUserDto );
   }
 
+  @Get('private')
+  @RoleProtected( ValidRoles.admin )
+  @UseGuards( AuthGuard(), UserRoleGuard )
+  testingPrivateRoute(
+    @GetUser() user: User
+  ){
+    return {
+      message: 'Hola Mundo Private',
+      user
+    }
+  }
+
+
   
 }
+
+
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,7 @@ export class AuthService {
 
       return {
         ...user,
-        token: this.getJwtToken({ email: user.email })
+        token: this.getJwtToken({ id: user.id })
       };
     }catch(error){
       console.log(error);
@@ -45,7 +45,7 @@ export class AuthService {
 
     const user = await this.userRepository.findOne({ 
       where: { email },
-      select: { email: true, password: true }
+      select: { email: true, password: true, id: true }
      });
 
     if( !user ) 
@@ -56,7 +56,7 @@ export class AuthService {
     
     return {
       ...user,
-      token: this.getJwtToken( { email: user.email} ) 
+      token: this.getJwtToken( { id: user.id } ) 
     };
     
   }
